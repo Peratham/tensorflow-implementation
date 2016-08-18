e19eb33c49575246d511a2f9dc86b4cf05b6445b
yunjey <yunjey@naver.com>
2016-08-18 21:52:03 +0900

    Some lstm forward prop functions ard added.

--- image-captioning/rnn.py
+++ image-captioning/rnn.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from rnn_layers import word_embedding_forward, rnn_forward, affine_forward
+from rnn_layers import word_embedding_forward, rnn_forward, lstm_forward, affine_forward
 from rnn_layers import temporal_affine_forward, temporal_softmax_loss
 
 """
@@ -15,8 +15,14 @@ H is dimension of hidden state.
 class ImageCaptioning(object):
 
 
-    def __init__(self, word_to_idx, batch_size= 100, dim_feature=512, dim_embed=128, dim_hidden=128, n_time_step=None, dtype=tf.float32):
+    def __init__(self, word_to_idx, batch_size= 100, dim_feature=512, dim_embed=128, 
+                    dim_hidden=128, n_time_step=None, cell_type='rnn', dtype=tf.float32):
+
+        if cell_type not in {'rnn', 'lstm'}:
+            raise ValueError('Invalid cell_type "%s"' % cell_type)
+
         # initialize ..
+        self.cell_type = cell_type
         self.word_to_idx = word_to_idx
         self.idx_to_word = {i: w for w, i in word_to_idx.iteritems()}
         self.V = len(word_to_idx)
@@ -40,10 +46,10 @@ class ImageCaptioning(object):
         self.params['b_proj'] = tf.Variable(tf.zeros([self.H]), name='b_proj')
 
         # Initialize parameters for the RNN
-        #dim_mul = {'lstm': 4, 'rnn': 1}[cell_type]
-        self.params['Wx'] = tf.Variable(tf.truncated_normal([self.M, self.H], stddev=0.1), name= 'Wx')
-        self.params['Wh'] = tf.Variable(tf.truncated_normal([self.H, self.H], stddev=0.1), name= 'Wh')
-        self.params['b'] = tf.Variable(tf.zeros([self.H]), name='b')
+        dim_mul = {'lstm': 4, 'rnn': 1}[cell_type]
+        self.params['Wx'] = tf.Variable(tf.truncated_normal([self.M, self.H * dim_mul], stddev=0.1), name= 'Wx')
+        self.params['Wh'] = tf.Variable(tf.truncated_normal([self.H, self.H * dim_mul], stddev=0.1), name= 'Wh')
+        self.params['b'] = tf.Variable(tf.zeros([self.H * dim_mul]), name='b')
 
         # Initialize output to vocab weights
         self.params['W_vocab'] = tf.Variable(tf.truncated_normal([self.H, self.V], stddev=0.1), name= 'W_vocab')
@@ -52,10 +58,14 @@ class ImageCaptioning(object):
         # Cast parameters to correct dtype
         for k, v in self.params.iteritems():
             self.params[k] = tf.cast(v, self.dtype)
+            
+        # Place holder for features and captions
+        self.features = tf.placeholder(tf.float32, [self.N, self.F])
+        self.captions = tf.placeholder(tf.int32, [self.N, self.T + 1])
 
-    def build_model(self, features, captions):
+    def build_model(self):
         """
-        Inputs:
+        Place Holder:
         - features: input image features of shape (N, F)
         - captions: ground-truth captions; an integer array of shape (N, T) where
           each element is in the range [0, V)
@@ -64,12 +74,23 @@ class ImageCaptioning(object):
         - logits: score of shape (N, T, V)
         - loss: Scalar loss
         """
+        # some hyper-parameters
+        T = self.T
+        N = self.N
+        V = self.V
+        H = self.H
+        M = self.M
+        F = self.F
+        
+        # place holder features and captions
+        features = self.features
+        captions = self.captions
 
         # caption in, out and mask matrix
-        captions_in = captions[:, :-1]
-        captions_out = captions[:, 1:]	    
-        mask = (captions_out != self._null)
-
+        captions_in = captions[:, :T]      # same as captions[:, :-1], tensorflow doesn't provide negative stop slice yet.
+        captions_out = captions[:, 1:]  
+        mask = tf.not_equal(captions_out, self._null)
+        
         # word embedding matrix
         W_embed = self.params['W_embed'] 
 
@@ -86,13 +107,6 @@ class ImageCaptioning(object):
         W_vocab = self.params['W_vocab'] 
         b_vocab = self.params['b_vocab'] 
 
-        # some hyper-parameters
-        T = self.T
-        N = self.N
-        V = self.V
-        H = self.H
-        M = self.M
-        F = self.F
 
         # params used in some function call
         rnn_param = {'n_time_step': T}
@@ -105,7 +119,10 @@ class ImageCaptioning(object):
         x = word_embedding_forward(captions_in, W_embed)  # (N, T, M)
 
         # rnn forward
-        h = rnn_forward(x, h0, Wx, Wh, b, rnn_param)  # (N, T, H)
+        if self.cell_type == 'rnn':
+            h = rnn_forward(x, h0, Wx, Wh, b, rnn_param)
+        else: 
+            h = lstm_forward(x, h0, Wx, Wh, b, rnn_param)
 
         # hidden-to-vocab
         logits = temporal_affine_forward(h, W_vocab, b_vocab, param)
