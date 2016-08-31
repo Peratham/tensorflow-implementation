48bb58d3dfbace0b54d320972cafb3e7c58ccacc
yunjey <yunjey47@naver.com>
2016-08-31 21:34:52 +0900

    utils added

---
+++ show_attend_and_tell/data/utils.py
@@ -0,0 +1,22 @@
+def decode_captions(captions, idx_to_word):
+    """
+    Inputs:
+    - captions: numpy ndarray where the value(word index) is in the range [0, V) of shape (N, T)
+    - idx_to_word: index to word mapping dictionary
+
+    Returns:
+    - decoded: decoded captions of shape (N, )
+    """
+    N, T = captions.shape
+    decoded = []
+
+    for i in range(N):
+        words = []
+        for t in range(T):
+            word = idx_to_word[captions[i, t]]
+            if word != '<NULL>':
+                words.append(word)
+            if word == '<END>':
+                break
+        decoded.append(' '.join(words))
+    return decoded
